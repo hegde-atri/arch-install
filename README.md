b851781edad3b2d9190c04b5e235b9e252dc388c
hegde-atri <iamatrihegde@gmail.com>
2022-04-03 18:25:46 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Arch installer
+
+You can install arch with either KDE-plasma or DWM, with the option to have my config files
---
+++ arch_install.sh
@@ -0,0 +1,7 @@
+# part1: make necassary partitions and chroot into it
+
+# part2: configuration
+
+# part3: choose DE/WM
+
+# part4: apply config
